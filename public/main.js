40dd6a20e6d60f910bfe2ee9974074f3eb1db8b0
WeiHua Wu <weihua.wu@digitalskillsforall>
2026-03-06 15:27:38 -0800

    api test V3

--- public/main.js
+++ public/main.js
@@ -2,7 +2,9 @@ async function apiSearch(searchTerm) {
     const netlifyFunctionURL = "/.netlify/functions/search";
     const options = {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: JSON.stringify({
             searchTerm: searchTerm
         })
@@ -12,5 +14,23 @@ async function apiSearch(searchTerm) {
     const data = await response.json();
 
     console.log("API result:", data);
+    return data;
 }
-apiSearch("panthera tigris");
+
+function displaySpecies(facts) {
+    document.querySelector(".card-name").innerHTML =
+        facts.scientific_name || "No name found";
+
+    document.querySelector(".card-text").innerHTML = `
+        "Category: " + ${facts.category || "Unknown"}
+        "Population trend: " + ${facts.population_trend || "Unknown"}
+        "Habitat: " + ${facts.habitat || "No habitat info"}
+        "Threats: " + ${facts.threats || "No threat info"}`;
+}
+
+async function searchAndDisplay(term) {
+    const speciesData = await apiSearch(term);
+    displaySpecies(speciesData);
+}
+
+searchAndDisplay();
